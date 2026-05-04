3d2d1518cd960d653c439f8ce8f6fcfc00897d04
wahyu_masrun <wahyumasrun9@gmail.com>
2026-05-04 16:27:15 +0700

    Add files via upload

---
+++ pertama.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Belajar PHP</title>
+</head>
+<body>
+	 <?php
+        echo " <h1>Selamat Datang ...</h1>";
+        echo "Program PHPku yang Pertama<br>";
+    ?>
+</body>
+</html>
